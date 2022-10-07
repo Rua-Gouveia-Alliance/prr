c0a44ba755d223c9fb2921f00e3278db5aced4d4
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-07 13:34:59 +0100

    Create Gold ClientType state

---
+++ prr-core/src/prr/clients/Gold.java
@@ -0,0 +1,22 @@
+package prr.clients;
+
+import prr.comunications.CommunicationType;
+
+public class Gold extends ClientType {
+    public Gold(Client client) {
+        super(client);
+    }
+    public ClientType getPrevType(){
+        return new Normal(client);
+    }
+    public ClientType getNextType(){
+        return new Platinum(client);
+    }
+    public boolean canDowngrade(){
+        return client.getBalance() < 0;
+    }
+    public boolean canUpgrade(){
+        return client.getBalance() >= 0 && client.straightCommunications(CommunicationType.VIDEO) == 5;
+        // a 5a ainda tem que ser considerado gold
+    }
+}
