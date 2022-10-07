0b01b98c446f976f8a0925042a9328ec09af3db0
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-07 13:36:28 +0100

    Create billing plan interface

---
+++ prr-core/src/prr/clients/ClientPlan.java
@@ -0,0 +1,12 @@
+package prr.clients;
+
+/* O clientType vai se atualizando sozinho por ser uma maquina de estatdo,
+ * a ideia e ir fazendo intanceof no objeto na altura de calcular os precos.
+ * Meter uma data de finals nos descendentes de clientPlan?
+ */
+public interface ClientPlan {
+    public long calcTextPrice(Communication communication);
+    public long calcVoicePrice(Communication communication);
+    public long calcVideoPrice(Communication communication);
+    public long calcPrice(Communication communication);
+}
