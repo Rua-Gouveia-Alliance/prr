7d93d7c16ef724f77c825378d91e45c1e1319214
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-07 13:34:02 +0100

    Start creating client functions

--- prr-core/src/prr/clients/Client.java
+++ prr-core/src/prr/clients/Client.java
@@ -1,9 +1,43 @@
 package prr.clients;
+import java.util.ArrayList;
 
 public class Client {
-    String id;
     String name;
+    String id;
     int nif;
-    ClientPlan plan;
-    boolean activeNotifications;
+    ClientPlan plan = new BasePlan();
+    ArrayList<Terminal> terminals = new ArrayList<>();
+    ClientType type = new Normal(this);
+    boolean notifications = false;
+    
+    public Client(String name, String id, int nif) {
+        this.name = name;
+        this.id = id;
+        this.nif = nif;
+    }
+
+    public void setType(ClientType type) {
+        this.type = type;
+    }
+    public void setNotifications(boolean notifications) {
+        this.notifications = notifications;
+    }
+    public long getOwed() {
+        long owed = 0;
+        for (Terminal terminal : terminals)
+            balance += terminal.getOwed();
+        return owed;
+    }
+    public long getPaid() {
+        long paid = 0;
+        for (Terminal terminal : terminals)
+            balance += terminal.getPaid();
+        return paid;
+    }
+    public long getBalance() {
+        return getPaid() - getOwed();
+    }
+    public long calcPrice(Communication communication) {
+        return plan.calcPrice();
+    }
 }
