03dcef105b8bdf9e74a210e5292073e77e762b27
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-07 13:35:11 +0100

    Create Normal ClientType state

---
+++ prr-core/src/prr/clients/Normal.java
@@ -0,0 +1,19 @@
+package prr.clients;
+
+public class Normal extends ClientType {
+    public Normal(Client client) {
+        super(client);
+    }
+    public ClientType getPrevType(){
+        return new Normal(client);
+    }
+    public ClientType getNextType(){
+        return new Gold(client);
+    }
+    public boolean canDowngrade(){
+        return false;
+    }
+    public boolean canUpgrade(){
+        return client.getBalance() > 500;
+    }
+}
