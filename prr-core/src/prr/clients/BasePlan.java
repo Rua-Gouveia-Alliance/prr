43eb5a671462c95bdf047250729c07268432a2e5
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-07 13:36:10 +0100

    Create default billing plan

---
+++ prr-core/src/prr/clients/BasePlan.java
@@ -0,0 +1,32 @@
+package prr.clients;
+
+public class BasePlan implements ClientPlan {
+    public long calcTextPrice(Communication communication) {
+        if (communication.getDuration() < 50)
+            return (communication.getClientType() == ClientTypeEnum.PLATINUM) ? 0 : 10;
+        if (communication.getDuration() < 100) {
+            if (communication.getClientType() == ClientTypeEnum.NORMAL) return 16;
+            if (communication.getClientType() == ClientTypeEnum.GOLD) return 10;
+            return 4;
+        }
+        return (communication.getClientType() == ClientTypeEnum.PLATINUM) ? 4 : 2 * communication.duration;
+    }
+    public long calcVoicePrice(Communication communication) {
+        return (communication.getClientType() == ClientTypeEnum.NORMAL) ? 20 : 10;
+    }
+    public long calcVideoPrice(Communication communication) {
+        if (communication.getClientType() == ClientTypeEnum.NORMAL) return 30;
+        if (communication.getClientType() == ClientTypeEnum.GOLD) return 20;
+        return 10;
+    }
+    public long calcPrice(Communication communication) {
+        switch (communication.getType()) {
+            case CommunicationType.TEXT:
+                return calcTextPrice(communication);
+            case CommunicationType.VOICE:
+                return calcVoicePrice(communication);
+            default:
+                return calcVideoPrice(communication);
+        }
+    }
+}
