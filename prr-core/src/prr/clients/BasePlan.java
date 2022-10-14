46c75753244dd9827b5f25a34f4c4a00db345747
João Gouveia <jjtucki4@gmail.com>
2022-10-14 17:40:38 +0100

    Add client attribute

--- prr-core/src/prr/clients/BasePlan.java
+++ prr-core/src/prr/clients/BasePlan.java
@@ -4,30 +4,36 @@ import prr.Communications.Communication;
 import prr.Communications.CommunicationType;
 
 public class BasePlan implements ClientPlan {
+    private Client client;
+
+    public BasePlan(Client client) {
+        this.client = client;
+    }
+
     @Override
     public long calcTextPrice(Communication communication) {
         if (communication.getLength() < 50)
-            return (communication.getClientType() instanceof Platinum) ? 0 : 10;
+            return (client.getType() instanceof Platinum) ? 0 : 10;
         if (communication.getLength() < 100) {
-            if (communication.getClientType() instanceof Gold)
+            if (client.getType() instanceof Gold)
                 return 10;
-            if (communication.getClientType() instanceof Normal)
+            if (client.getType() instanceof Normal)
                 return 16;
             return 4;
         }
-        return (communication.getClientType() instanceof Platinum) ? 4 : 2 * communication.GetDuration();
+        return (client.getType() instanceof Platinum) ? 4 : 2 * communication.GetDuration();
     }
 
     @Override
     public long calcVoicePrice(Communication communication) {
-        return (communication.getClientType() instanceof Normal) ? 20 : 10;
+        return (client.getType() instanceof Normal) ? 20 : 10;
     }
 
     @Override
     public long calcVideoPrice(Communication communication) {
-        if (communication.getClientType() instanceof Normal)
+        if (client.getType() instanceof Normal)
             return 30;
-        if (communication.getClientType() instanceof Gold)
+        if (client.getType() instanceof Gold)
             return 20;
         return 10;
     }
