41d995e8afdb3d8f4df6bb697bd42c509f4cb055
João Gouveia <jjtucki4@gmail.com>
2022-10-14 21:54:56 +0100

    Add natural language text comparator to tree maps and create communication key getter

--- prr-core/src/prr/Network.java
+++ prr-core/src/prr/Network.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import prr.app.exceptions.DuplicateClientKeyException;
 import prr.clients.Client;
 import prr.exceptions.UnrecognizedEntryException;
+import prr.util.NaturalLanguageTextComparator;
 
 /**
  * Class Store implements a store.
@@ -18,15 +19,34 @@ public class Network implements Serializable {
     private static final long serialVersionUID = 202208091753L;
 
     /** Clients list, sorted by key */
-    /** FIXME Add proper text comparator to TreeMap */
-    private final Map<String, Client> clients = new TreeMap<>();
+    private final Map<String, Client> clients = new TreeMap<>(new NaturalLanguageTextComparator());
 
     /** Terminals list, sorted by key */
-    /** FIXME Add proper text comparator to TreeMap */
-    private final Map<String, Client> terminals = new TreeMap<>();
+    private final Map<String, Client> terminals = new TreeMap<>(new NaturalLanguageTextComparator());
 
-    /** Communications counter, used for communication IDs */
-    private int communicationCount = 0;
+    /** Communications counter, used for generating communication keys */
+    private int communicationKey = 0;
+
+    /**
+     * Get and increment the communication key tracker
+     */
+    public int getCommunicationKey() {
+        return communicationKey++;
+    }
+
+    /**
+     * Register a new client in the network
+     * 
+     * @param name new client's name
+     * @param key  new client's key
+     * @param nif  new client's tax id
+     * @throws DuplicateClientKeyException if the given key is already in use
+     */
+    public void newClient(String name, String key, int nif) throws DuplicateClientKeyException {
+        if (clients.containsKey(key))
+            throw new DuplicateClientKeyException(key);
+        clients.put(key, new Client(name, key, nif));
+    }
 
     /**
      * Read text input file and create corresponding domain entities.
@@ -38,19 +58,5 @@ public class Network implements Serializable {
      */
     void importFile(String filename) throws UnrecognizedEntryException, IOException /* FIXME maybe other exceptions */ {
         // FIXME implement method
-    }
-
-    /**
-     * Read text input file and create corresponding domain entities.
-     * 
-     * @param name new client's name
-     * @param key  new client's key
-     * @param nif  new client's tax id
-     * @throws DuplicateClientKeyException if the given key is already in use
-     */
-    public void newClient(String name, String key, int nif) throws DuplicateClientKeyException {
-        if (clients.containsKey(key))
-            throw new DuplicateClientKeyException(key);
-        clients.put(key, new Client(name, key, nif));
     }
 }
