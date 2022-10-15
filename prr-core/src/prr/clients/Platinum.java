bf52e164dc13d64beb5f3b6e9e3fee7d770cfb64
João Gouveia <jjtucki4@gmail.com>
2022-10-15 14:47:34 +0100

    Formatting

--- prr-core/src/prr/clients/Platinum.java
+++ prr-core/src/prr/clients/Platinum.java
@@ -29,7 +29,7 @@ public class Platinum extends ClientType {
     public boolean canUpgrade() {
         return false;
     }
-    
+
     @Override
     public String toString() {
         return "PLATINUM";
