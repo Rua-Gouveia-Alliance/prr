d324ccd2e2cde624a8418208742d99655c729100
João Gouveia <jjtucki4@gmail.com>
2022-10-15 16:26:55 +0100

    ClientExistsException now extends KeyException

--- prr-core/src/prr/exceptions/ClientExistsException.java
+++ prr-core/src/prr/exceptions/ClientExistsException.java
@@ -3,19 +3,10 @@ package prr.exceptions;
 /**
  * Exception thrown when trying to register a client with
  * a key that already exists.
- * Used to pass the duplicate key to the app.
  */
-public class ClientExistsException extends Exception {
-
-    private final String key;
-
+public class ClientExistsException extends KeyException {
     /** @param key the duplicated key */
     public ClientExistsException(String key) {
-        this.key = key;
-    }
-
-    /** @return The key that caused the error */
-    public String getKey() {
-        return this.key;
+        super(key);
     }
 }
