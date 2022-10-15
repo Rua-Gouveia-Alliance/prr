ebe89880b586c17a1947cd95ebae10aec1897dcf
João Gouveia <jjtucki4@gmail.com>
2022-10-15 16:24:57 +0100

    Implement execute

--- prr-app/src/prr/app/clients/DoShowClient.java
+++ prr-app/src/prr/app/clients/DoShowClient.java
@@ -2,9 +2,9 @@ package prr.app.clients;
 
 import prr.Network;
 import prr.app.exceptions.UnknownClientKeyException;
+import prr.exceptions.ClientDoesntExistException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Show specific client: also show previous notifications.
@@ -13,11 +13,15 @@ class DoShowClient extends Command<Network> {
 
     DoShowClient(Network receiver) {
         super(Label.SHOW_CLIENT, receiver);
-        // FIXME add command fields
+        addStringField("key", Prompt.key());
     }
 
     @Override
     protected final void execute() throws CommandException {
-        // FIXME implement command
+        try {
+            _display.popup(_receiver.getClient(stringField("key")));
+        } catch (ClientDoesntExistException e) {
+            throw new UnknownClientKeyException(e.getKey());
+        }
     }
 }
