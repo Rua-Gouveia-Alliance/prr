f915f1bf86335542b1fc1724489614996aaaa001
Joao Gouveia <jjtucki4@gmail.com>
2022-10-15 21:50:24 +0100

    Fix imports

--- prr-app/src/prr/app/terminals/DoShowAllTerminals.java
+++ prr-app/src/prr/app/terminals/DoShowAllTerminals.java
@@ -1,11 +1,10 @@
 package prr.app.terminals;
 
-import java.util.Collections;
-
 import prr.Network;
+import prr.terminals.Terminal;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
+import java.util.Collection;
 
 /**
  * Show all terminals.
