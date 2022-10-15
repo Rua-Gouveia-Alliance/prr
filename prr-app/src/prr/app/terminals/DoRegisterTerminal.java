4bec7104a614a772e62a1700a7fbbf08f0f818a3
Joao Gouveia <jjtucki4@gmail.com>
2022-10-15 21:50:57 +0100

    Fix imports and add exceptions and error handling

--- prr-app/src/prr/app/terminals/DoRegisterTerminal.java
+++ prr-app/src/prr/app/terminals/DoRegisterTerminal.java
@@ -7,7 +7,7 @@ import prr.app.exceptions.UnknownClientKeyException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import prr.exceptions.ClientDoesntExistException;
-//FIXME add more imports if needed
+import prr.exceptions.IncorrectTerminalKeyException;
 import prr.exceptions.TerminalExistsException;
 
 /**
@@ -20,18 +20,21 @@ class DoRegisterTerminal extends Command<Network> {
         // command fields
         addStringField("key", Prompt.terminalKey());
         addStringField("type", Prompt.terminalType());
+        while (!(stringField("type").equals("FANCY") || stringField("type").equals("BASIC")))
+            addStringField("type", Prompt.terminalType());
         addStringField("clientKey", Prompt.clientKey());
     }
 
     @Override
     protected final void execute() throws CommandException {
         try {
-            // TODO: check if key is valid
             _receiver.registerTerminal(stringField("key"), stringField("type"), stringField("clientKey"));
         } catch (TerminalExistsException e) {
             throw new DuplicateTerminalKeyException(e.getKey());
         } catch (ClientDoesntExistException e) {
             throw new UnknownClientKeyException(e.getKey());
+        } catch (IncorrectTerminalKeyException e) {
+            throw new InvalidTerminalKeyException(e.getKey());
         }
     }
 }
