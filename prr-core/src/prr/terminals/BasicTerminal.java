c3dc45c12ec7be2e297950714c13ea4efc19ca0c
Joao Gouveia <jjtucki4@gmail.com>
2022-10-15 21:01:49 +0100

    Imports

--- prr-core/src/prr/terminals/BasicTerminal.java
+++ prr-core/src/prr/terminals/BasicTerminal.java
@@ -1,5 +1,7 @@
 package prr.terminals;
 
+import prr.clients.Client;
+
 public class BasicTerminal extends Terminal {
 
     public BasicTerminal(String key, Client owner) {
--- prr-core/src/prr/terminals/FancyTerminal.java
+++ prr-core/src/prr/terminals/FancyTerminal.java
@@ -1,5 +1,7 @@
 package prr.terminals;
 
+import prr.clients.Client;
+
 public class FancyTerminal extends Terminal {
 
     public FancyTerminal(String key, Client owner) {
