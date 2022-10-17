1bb84bd430514f3d34d887393aef1f28006a93c9
Joao Gouveia <jjtucki4@gmail.com>
2022-10-17 13:56:00 +0100

    Change constructor

--- prr-core/src/prr/terminals/BasicTerminal.java
+++ prr-core/src/prr/terminals/BasicTerminal.java
@@ -1,11 +1,12 @@
 package prr.terminals;
 
 import prr.clients.Client;
+import prr.terminals.states.TerminalState;
 
 public class BasicTerminal extends Terminal {
 
-    public BasicTerminal(String key, Client owner) {
-        super(key, owner);
+    public BasicTerminal(String key, Client owner, TerminalState state) {
+        super(key, owner, state);
     }
 
     @Override
--- prr-core/src/prr/terminals/FancyTerminal.java
+++ prr-core/src/prr/terminals/FancyTerminal.java
@@ -1,11 +1,12 @@
 package prr.terminals;
 
 import prr.clients.Client;
+import prr.terminals.states.TerminalState;
 
 public class FancyTerminal extends Terminal {
 
-    public FancyTerminal(String key, Client owner) {
-        super(key, owner);
+    public FancyTerminal(String key, Client owner, TerminalState state) {
+        super(key, owner, state);
     }
 
     @Override
