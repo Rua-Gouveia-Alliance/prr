ca7cac6bb4ed2f5877426b3366adb731e44954ad
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-21 00:22:03 +0100

    Implement DoDisableClientNotifications

--- prr-app/src/prr/app/clients/DoDisableClientNotifications.java
+++ prr-app/src/prr/app/clients/DoDisableClientNotifications.java
@@ -2,9 +2,10 @@ package prr.app.clients;
 
 import prr.Network;
 import prr.app.exceptions.UnknownClientKeyException;
+import prr.exceptions.ClientDoesntExistException;
+import prr.exceptions.NotificationsAlreadyDisabledException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Disable client notifications.
@@ -13,11 +14,17 @@ class DoDisableClientNotifications extends Command<Network> {
 
     DoDisableClientNotifications(Network receiver) {
         super(Label.DISABLE_CLIENT_NOTIFICATIONS, receiver);
-        // FIXME add command fields
+        addStringField("key", Prompt.key());
     }
 
     @Override
     protected final void execute() throws CommandException {
-        // FIXME implement command
+        try {
+            _receiver.disableNotifications(stringField("key"));
+        } catch (ClientDoesntExistException e) {
+            throw new UnknownClientKeyException(e.getKey());
+        } catch (NotificationsAlreadyDisabledException e) {
+            _display.popup(Message.clientNotificationsAlreadyDisabled());
+        }
     }
 }
