397e31e89320cbaad61bb4bfc0ec470a35d9fcb9
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-21 00:22:39 +0100

    Implement DoShowClientPaymentsAndDebts

--- prr-app/src/prr/app/clients/DoShowClientPaymentsAndDebts.java
+++ prr-app/src/prr/app/clients/DoShowClientPaymentsAndDebts.java
@@ -1,10 +1,10 @@
 package prr.app.clients;
 
 import prr.Network;
+import prr.exceptions.ClientDoesntExistException;
 import prr.app.exceptions.UnknownClientKeyException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Show the payments and debts of a client.
@@ -13,11 +13,17 @@ class DoShowClientPaymentsAndDebts extends Command<Network> {
 
     DoShowClientPaymentsAndDebts(Network receiver) {
         super(Label.SHOW_CLIENT_BALANCE, receiver);
-        // FIXME add command fields
+        addStringField("key", Prompt.key());
     }
 
     @Override
     protected final void execute() throws CommandException {
-        // FIXME implement command
+        try {
+            String key = stringField("key");
+            _display.popup(Message.clientPaymentsAndDebts(key, _receiver.getClientPayments(key),
+                    _receiver.getClientDebt(key)));
+        } catch (ClientDoesntExistException e) {
+            throw new UnknownClientKeyException(e.getKey());
+        }
     }
 }
