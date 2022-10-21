df6f2779308d323c6db61b1bdc7623eb75b36d74
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-21 19:35:25 +0100

    Remove useless serial uid

--- prr-core/src/prr/notifications/Notification.java
+++ prr-core/src/prr/notifications/Notification.java
@@ -1,9 +1,6 @@
 package prr.notifications;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public record Notification(NotificationType type, int terminalId) implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 202217101700L;
 }
