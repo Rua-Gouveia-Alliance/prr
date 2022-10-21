f20cbd560255eedd833a171bce0e653d05492ca5
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-10-21 19:33:59 +0100

    Notification now is a record

--- prr-core/src/prr/notifications/Notification.java
+++ prr-core/src/prr/notifications/Notification.java
@@ -3,25 +3,7 @@ package prr.notifications;
 import java.io.Serial;
 import java.io.Serializable;
 
-public class Notification implements Serializable {
-    
+public record Notification(NotificationType type, int terminalId) implements Serializable {
     @Serial
     private static final long serialVersionUID = 202217101700L;
-    
-    private NotificationType type;
-    private int terminalId;
-
-    public Notification(NotificationType type, int terminalId) {
-        this.type = type;
-        this.terminalId = terminalId;
-    }
-    
-    public NotificationType getType() {
-        return type;
-    }
-
-    public int getTerminalId() {
-        return terminalId;
-    }
-
 }
